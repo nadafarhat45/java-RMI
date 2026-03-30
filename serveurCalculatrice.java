b154e6f8d3a9ad91e563dcb8a86d49c6042333d6
nada farhat <farhatn899@gmail.com>
2026-03-30 16:15:26 +0100

    Add files via upload

---
+++ serveurCalculatrice.java
@@ -0,0 +1,12 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class serveurCalculatrice {
+    public static void main(String[] args)  throws Exception {
+            ICalculatrice calculatrice = new CalculatriceImpl();
+            Registry registry = LocateRegistry.createRegistry(1099);
+            registry.rebind("CalculatriceService", calculatrice);
+            System.out.println("Serveur de Calculatrice prêt...");
+         }
+}
+
